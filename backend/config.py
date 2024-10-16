61a8dc20c454684d3bebcbd3770b046f8c75eff0
Jatinbhardwaj-093 <bhardwajjatin093@gmail.com>
2024-10-16 20:20:32 +0530

    Added path for  services image storing

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'Static/services/') 
