bde35e344b6971c86d0d3cfc2658a8b17a9f01d9
Jatinbhardwaj-093 <bhardwajjatin093@gmail.com>
2024-10-22 17:13:29 +0530

    customer signup api

--- backend/api/customerApi.py
+++ backend/api/customerApi.py
@@ -8,7 +8,7 @@ customerApi = Blueprint('customerApi', __name__)
 @customerApi.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
-    founduser = Customer.query.filter_by(email=data['email']).first()
+    founduser = Customer.query.filter_by(username = data['username']).first()
     if founduser:
         return jsonify({'message': 'User already exists'}), 409
     else:
@@ -19,9 +19,25 @@ def signup():
             password=password,
             contact=data['contact'],
             pincode=data['pincode'],
-            Address=data['Address']
+            Address=data['address']
         )
         db.session.add(newuser)
         db.session.commit()
         return jsonify({'message': 'User created successfully'}), 201
-    
+
+# Customer Home Api
+
+@customerApi.route('/', methods=['GET'])
+def home():
+    services = Services.query.all()
+    response = []
+    for service in services:
+        img = ServiceImg.query.filter_by(Service_id=service.id).first()
+        response.append({
+            'id': service.id,
+            'description': service.description,
+            'price': service.price,
+            'name': service.serviceName,
+            'image': img.filepath if img else None
+        })
+    return jsonify(response)
