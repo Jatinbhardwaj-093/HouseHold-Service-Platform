fed2a9f819fb41e1e76c020eb5b632af08ac1f08
Jatin Bhardwaj <jatinbhardwaj@Jatins-MacBook-Air.local>
2024-11-11 19:00:27 +0530

    professional history of service request page and website home page

---
+++ frontend/src/views/Professional_HistoryView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ProfessionalNavbar from '@/components/Professional/ProfessionalNavbar.vue';
+import ProfessionalHistory from '@/components/Professional/ProfessionalHistory.vue';
+</script>
+
+<template>
+    <div>
+        <ProfessionalNavbar/>
+        <br>
+        <ProfessionalHistory/>
+    </div>
+</template>
---
+++ frontend/src/views/WebsiteHomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        <h1 class="heading">Welcome to Home Service App</h1>
+        <router-link :to="{name: 'login'}" class="btn">Login</router-link>
+        <router-link :to="{name: 'signup'}" class="btn">Sign UP</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+.heading {
+    text-align: center;
+    font-weight: bolder;
+    font-size: 3.5rem;
+    margin-top: 1.5rem;
+    margin-bottom: 3rem;
+    font-weight: bolder;
+    font-style: italic;
+    color: #FF5C01;
+}
+
+.btn{
+    font-size: 1.5rem;
+    padding: .5rem;
+    border-radius: .5rem;
+    border: none;
+    cursor: pointer;
+    background-color: #FF5C01;
+    color: white;
+    margin: 0.5rem;
+    text-decoration: none;
+}
+
+</style>
