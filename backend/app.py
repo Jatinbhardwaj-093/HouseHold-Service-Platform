28c7317e33bc036668e48259332efe2f376f890c
Jatin Bhardwaj <148186488+Jatinbhardwaj-093@users.noreply.github.com>
2024-11-23 18:36:28 +0530

    init for email notifications

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate
 from models import db 
 from flask_cors import CORS
 
-
 # Api Imports
 from api.loginApi import loginApi  
 from api.customerApi import customerApi
@@ -14,6 +13,10 @@ from config import Config
 # bcrypt and jwt instance importing
 from api import bcrypt, jwt_manager, redis_client
 
+# Mail imports
+from extension import mail
+from notifications.email_scheduler import schedule_email
+
 def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True)
@@ -23,10 +26,13 @@ def create_app():
     db.init_app(app)  # Initialize the db with the app
     jwt_manager.init_app(app)  # Initialize the JWT library
     migrate = Migrate(app, db)  # Initialize Flask-Migrate
+    mail.init_app(app)  # Initialize the Mail library
 
     app.register_blueprint(loginApi)  # Register the login API
     app.register_blueprint(customerApi, url_prefix='/customer')  # Register the customer API
     app.register_blueprint(professionalApi, url_prefix='/professional')  # Register the professional API
     app.register_blueprint(adminApi, url_prefix='/admin')  # Register the admin API
-
+    
+    with app.app_context():
+        schedule_email(app)
     return app
